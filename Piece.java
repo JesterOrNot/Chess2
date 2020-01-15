e29f2bef3336640cce9ad8cddf0c056d4c4720d5
Sean Hellum <seanhellum45@gmail.com>
2020-01-15 16:38:14 +0000

    add constructor

--- Piece.java
+++ Piece.java
@@ -3,7 +3,16 @@
  */
 public abstract class Piece {
     private boolean isWhite;
+    public char icon;
     private boolean isFirstMove;
+
     public abstract boolean validMoves();
+
     public abstract void move();
+
+    public Piece(char icon, boolean isWhite) {
+        this.isWhite = isWhite;
+        this.isFirstMove = true;
+        this.icon = icon;
+    }
 }
