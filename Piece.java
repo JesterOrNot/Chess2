5a516d2796224a51cb61738838583d7ca32738bc
Sean Hellum <seanhellum45@gmail.com>
2020-01-15 16:40:51 +0000

    getters and setters for Piece,java

--- Piece.java
+++ Piece.java
@@ -3,16 +3,49 @@
  */
 public abstract class Piece {
     private boolean isWhite;
-    public char icon;
+    private int value;
+    private char icon;
     private boolean isFirstMove;
 
     public abstract boolean validMoves();
 
+    public char getIcon() {
+        return icon;
+    }
+
+    public void setIcon(char icon) {
+        this.icon = icon;
+    }
+
+    public boolean isFirstMove() {
+        return isFirstMove;
+    }
+
+    public void setFirstMove(boolean isFirstMove) {
+        this.isFirstMove = isFirstMove;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public boolean isWhite() {
+        return this.isWhite;
+    }
+
+    public void setWhite(boolean isWhite) {
+        this.isWhite = isWhite;
+    }
+
     public abstract void move();
 
     public Piece(char icon, boolean isWhite) {
-        this.isWhite = isWhite;
-        this.isFirstMove = true;
-        this.icon = icon;
+        this.setWhite(isWhite);
+        this.setFirstMove(true);
+        this.setIcon(icon);
     }
 }
